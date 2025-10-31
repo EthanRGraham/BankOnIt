88111c78d0d7adfc59596df47f4ac1a660f9b60c
EthanRGraham <ethan.graham@bsu.edu>
2025-10-31 01:48:23 +0000

    got CheckingAmmount to test run!

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,7 +1,9 @@
-import java.util.Scanner;
 
-class CheckingAccount implements HasMenu {
-	private double balance = 100.00; // initial starting balance
+import java.util.*;
+import java.io.*;
+
+class CheckingAccount implements HasMenu, Serializable {
+	double balance;
 	
 	public static void main(String[] args){
 		CheckingAccount ca = new CheckingAccount();
@@ -16,61 +18,96 @@ class CheckingAccount implements HasMenu {
 	System.out.println("3 Make Withdrawal");
         System.out.print("Enter your choice: ");
 	System.out.println("\n=== Checking Account Menu ===");
-	String response = scanner.nextLine(); // get input as a string
-        return response;
+       	
+
+	Scanner scanner = new Scanner(System.in);
+
+	String response = scanner.nextLine(); 
+	return response;
 	}// end Menu()
 	
 	public void start(){
 		boolean keepGoing = true;
-		string choice = Menu();
 		
-		/*Scanner scanner = new Scanner(System.in);
-
 		while (keepGoing) {
-			Menu();
-			int choice = scanner.nextInt();
+			String response = Menu();
 
-			if (choice == 0) {
+			if (response.equals("0")) {
 				System.out.println("Goodbye");
 				keepGoing = false;
 			}
-		       if (choice == 1) {
+		       if (response.equals("1")) {
 			       this.checkBalance();
 		       }
-		       else if (choice == 2) {
-                               this.makeDeposit(scanner);
+		       else if (response.equals("2")) {
+                               this.makeDeposit();
                        }
-		       else if (choice == 3) {
-			       this.makeWithdrawal(scanner);
+		       else if (response.equals("3")) {
+			       this.makeWithdrawal();
 		       }
 		       else {
 			       System.out.println("Please enter 0, 1, 2, or 3.");
-		       }
-		}// end keepGoing
-		scanner.close(); 
+		       } //end if
+		}// end keepGoing 
 	} // end start()
-*/
-	public void makeDeposit(Scanner scanner) {
+	
+
+	public void makeDeposit() {
 	       	System.out.print("Enter the amount to deposit: ");
+		double deposit = this.getDouble();
+		this.balance += deposit;
+		System.out.println("New Balance: $" + this.getBalanceString());
+	}// end makeDeposit
+	
+
 		/*
         	double amount = scanner.nextDouble();
         	balance += amount;
         	System.out.println("You deposited $" + amount + ". New balance: $" + balance);*/
-    }
+    
 
-    	public void makeWithdrawal(Scanner scanner) {
+    	public void makeWithdrawal() {
 	   	System.out.print("Enter the amount to withdraw: ");
-	    	/*double amount = scanner.nextDouble();
+	    	double withdrawal = this.getDouble();
+		if (withdrawal > this.balance){
+		       System.out.println("Insufficient funds");
+		} else {
+			this.balance -= withdrawal;
+		} // end if
+		System.out.println("New balance:" + this.getBalanceString());
+	} //end makeWithdrawal
+
+		/*double amount = scanner.nextDouble();
 
 	    	if (amount <= 0) {
 			System.out.print("Withdrawal amount must be positive");
 	    	}
 	    	else if (amount > balance) {
 			System.out.print();*/ 
-    }
 
-    	public void checkBalance() {
+    	public void checkBalance(){
 		System.out.print("You're in checkBalance()");
-	}
+	} 
+
+	public String getBalanceString(){
+		String result = String.format("$%.02f", this.balance);
+		return result;
+	}//end getBalanceString
+
+	private double getDouble() {
+    		Scanner input = new Scanner(System.in);
+   		 double result = 0.0;
+   		 try {
+        		System.out.print("Enter amount: ");
+        		String sResult = input.nextLine();
+        		result = Double.parseDouble(sResult);
+		 } catch (NumberFormatException e) {
+			 System.out.println("Invalid input. Please enter a numeric value.");
+		 }
+		 return result;
+	}// end getDouble
 
 }// end class CheckingAccount
+//
+
+
